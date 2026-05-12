d2912ec179048f4830ee592f98dc5c08f0a4c78b
Travis Haskin <thaskin@ualr.edu>
2026-05-12 03:18:28 +0000

    final menu and location pages

---
+++ final-project/assets/js/script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById('mobile-menu');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    
+    menuToggle.classList.toggle('is-active');
+});
